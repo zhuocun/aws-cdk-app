ff5311cd46a9d860edb90b89582f07fe7b691116
zhuocun <zhuocun.tang@outlook.com>
2023-06-19 10:51:02 +1000

    feat: initialised first version of lambda function

--- bin/aws-cdk-app.ts
+++ bin/aws-cdk-app.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
+import * as cdk from "@aws-cdk/core";
 import { AwsCdkAppStack } from "../lib/aws-cdk-app-stack";
 
 const app = new cdk.App();
--- lib/aws-cdk-app-stack.ts
+++ lib/aws-cdk-app-stack.ts
@@ -1,16 +1,10 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import { WidgetService } from "./widget-service";
 
 export class AwsCdkAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new WidgetService(this, "Widgets");
   }
 }
