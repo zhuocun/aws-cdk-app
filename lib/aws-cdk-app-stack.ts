dc108016e633da7b79b42e592157e37689f341d2
zhuocun <zhuocun.tang@outlook.com>
2023-06-21 11:27:14 +1000

    style: improved code style

--- lib/aws-cdk-app-stack.ts
+++ lib/aws-cdk-app-stack.ts
@@ -3,9 +3,8 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class AwsCdkAppStack extends Stack {
-    private infrastructure: WidgetService;
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
-        this.infrastructure = new WidgetService(this, "Widgets");
+        new WidgetService(this, "Widgets");
     }
 }
