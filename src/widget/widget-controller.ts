c8491a7d37b0fb5890ff9b199901cfdea48d0a48
zhuocun <zhuocun.tang@outlook.com>
2023-06-23 22:02:31 +1000

    feat: updated error message

--- src/widget/widget-controller.ts
+++ src/widget/widget-controller.ts
@@ -2,40 +2,40 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 import { createWidget, deleteWidget, getWidgets } from "./widget-service";
 
 const formatResponse = (
-    statusCode: number,
-    body: unknown
+  statusCode: number,
+  body: unknown
 ): { statusCode: number; headers: object; body: string } => {
-    return {
-        statusCode,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body)
-    };
+  return {
+    statusCode,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  };
 };
 
 const main = async (
-    event: APIGatewayProxyEvent
+  event: APIGatewayProxyEvent
 ): Promise<{ statusCode: number; headers: object; body: string }> => {
-    const method = event.httpMethod;
-    const widgetName = event.path?.startsWith("/")
-        ? event.path.substring(1)
-        : event.path;
-    try {
-        switch (method) {
-        case "GET":
-            return formatResponse(200, await getWidgets(widgetName));
-        case "POST":
-            return formatResponse(200, await createWidget(widgetName));
-        case "DELETE":
-            return formatResponse(200, await deleteWidget(widgetName));
-        default:
-            return formatResponse(
-                400,
-                "Only GET, POST, and DELETE requests are accepted now"
-            );
-        }
-    } catch (error) {
-        return formatResponse(500, error instanceof Error ? error.message : error);
+  const method = event.httpMethod;
+  const widgetName = event.path?.startsWith("/")
+    ? event.path.substring(1)
+    : event.path;
+  try {
+    switch (method) {
+      case "GET":
+        return formatResponse(200, await getWidgets(widgetName));
+      case "POST":
+        return formatResponse(200, await createWidget(widgetName));
+      case "DELETE":
+        return formatResponse(200, await deleteWidget(widgetName));
+      default:
+        return formatResponse(
+          400,
+          "Only GET, POST, and DELETE requests are accepted"
+        );
     }
+  } catch (error) {
+    return formatResponse(500, error instanceof Error ? error.message : error);
+  }
 };
 
 export { main };
