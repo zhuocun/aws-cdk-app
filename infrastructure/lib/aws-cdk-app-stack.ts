89dbebf3e5aad59476c70068ca3a0661826ccffa
zhuocun <zhuocun.tang@outlook.com>
2023-06-24 22:35:54 +1000

    feat: added jenkins into infrastructure

--- infrastructure/lib/aws-cdk-app-stack.ts
+++ infrastructure/lib/aws-cdk-app-stack.ts
@@ -1,12 +1,14 @@
 import { WidgetInfrastructure } from "./widget-infrastructure";
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { JenkinsInfrastructure } from "./jenkins-infrastructure";
 
 class AwsCdkAppStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
-        new WidgetInfrastructure(this, "Widgets");
-    }
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    new WidgetInfrastructure(this, "Widgets");
+    new JenkinsInfrastructure(this, "Jenkins");
+  }
 }
 
 export { AwsCdkAppStack };
